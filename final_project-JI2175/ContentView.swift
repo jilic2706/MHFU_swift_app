47c3c6d38eb6ed0da229ea12fa82c0b9cd072f72
Josip Ilic <ji2175@rit.edu>
2021-12-08 15:35:09 +0100

    Initial Commit

---
+++ final_project-JI2175/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  final_project-JI2175
+//
+//  Created by Illy Byos on 08.12.2021..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ final_project-JI2175/final_project_JI2175App.swift
@@ -0,0 +1,17 @@
+//
+//  final_project_JI2175App.swift
+//  final_project-JI2175
+//
+//  Created by Illy Byos on 08.12.2021..
+//
+
+import SwiftUI
+
+@main
+struct final_project_JI2175App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
